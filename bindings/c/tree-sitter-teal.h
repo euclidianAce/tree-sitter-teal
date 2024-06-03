d9a24c5d7b946cc755987ac09289f49191eea973
Corey Williamson <euclidianAce@protonmail.com>
2024-06-03 05:45:09 -0500

    Actually commit generated things and remove most of them from gitignore

---
+++ bindings/c/tree-sitter-teal.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_TEAL_H_
+#define TREE_SITTER_TEAL_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_teal(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_TEAL_H_
---
+++ bindings/python/tree_sitter_teal/__init__.py
@@ -0,0 +1,5 @@
+"Teal grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
