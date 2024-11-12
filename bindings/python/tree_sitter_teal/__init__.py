b2c9d0aef3cb97fd0ec91b721bcfe3a7d89add37
FourierTransformer <FourierTransformer@users.noreply.github.com>
2024-11-12 09:51:09 -0600

    updated rust and python bindings with queries

--- bindings/python/tree_sitter_teal/__init__.py
+++ bindings/python/tree_sitter_teal/__init__.py
@@ -14,12 +14,14 @@ def _get_query(name, file):
 def __getattr__(name):
     # NOTE: uncomment these to include any queries that this grammar contains:
 
-    # if name == "HIGHLIGHTS_QUERY":
-    #     return _get_query("HIGHLIGHTS_QUERY", "highlights.scm")
+    if name == "HIGHLIGHTS_QUERY":
+        return _get_query("HIGHLIGHTS_QUERY", "highlights.scm")
     # if name == "INJECTIONS_QUERY":
     #     return _get_query("INJECTIONS_QUERY", "injections.scm")
-    # if name == "LOCALS_QUERY":
-    #     return _get_query("LOCALS_QUERY", "locals.scm")
+    if name == "LOCALS_QUERY":
+        return _get_query("LOCALS_QUERY", "locals.scm")
+    if name == "FOLDS_QUERY":
+        return _get_query("FOLDS_QUERY", "folds.scm")
     # if name == "TAGS_QUERY":
     #     return _get_query("TAGS_QUERY", "tags.scm")
 
@@ -28,9 +30,10 @@ def __getattr__(name):
 
 __all__ = [
     "language",
-    # "HIGHLIGHTS_QUERY",
+    "HIGHLIGHTS_QUERY",
     # "INJECTIONS_QUERY",
-    # "LOCALS_QUERY",
+    "LOCALS_QUERY",
+    "FOLDS_QUERY",
     # "TAGS_QUERY",
 ]
 
